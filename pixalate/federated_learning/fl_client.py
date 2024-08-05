1137eec0c4729ca4245062621aa0a9f913274dcf
Abin (אבין ברגיס) <abinvarghese90@gmail.com>
2024-08-05 14:09:23 +0100

    ^.^

--- pixalate/federated_learning/fl_client.py
+++ pixalate/federated_learning/fl_client.py
@@ -2,4 +2,4 @@ import flwr as fl
 import torch 
 import torch.nn as nn
 import torch.optim as optim
-from pixalate.models.fraud_detection_model import create_model, get_model_parameters, set_model_parameters
+from pixalate.models.fraud_detection_model import FraudDetectionModel, create_model, get_model_parameters, set_model_parameters
--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 # Function to read the requirements.txt file
 def read_requirements():
